354adf95efcd321d65b3dd2cf3ef9deb81257113
henning jaeger <henning.jaeger@gmail.com>
2022-06-07 08:57:51 +0200

    credentials

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'MohamedAljahani',
+  appName: 'Ionic Voice Recorder with compression',
   webDir: 'www',
   bundledWebRuntime: false,
   cordova: {
